800908eb4c5eb060805c087245c7ae1f627f20ac
Asa Gayle <azmatch.gayle@gmail.com>
2025-06-03 17:00:24 -0400

    Added example test in options

---
+++ go_tmdb_suite_test.go
@@ -0,0 +1,13 @@
+package gotmdb_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestGoTmdb(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "GoTmdb Suite")
+}
--- options/options_test.go
+++ options/options_test.go
@@ -1,1 +1,22 @@
-package options
+package options_test
+
+import (
+	"github.com/aamgayle/gotmdb/options"
+	. "github.com/aamgayle/gotmdb/types"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Options tests", func() {
+	Context("When given a different base url", func() {
+		It("should return a url that doesn't match the default", func() {
+			protocol := "https://"
+			testHost := "www.testurl.com"
+			testSudDirectory := "/1/test/"
+
+			tmdbReq, err := NewTMDBRequest(options.WithBaseURL(protocol + testHost + testSudDirectory))
+			Expect(err).To(BeNil())
+			Expect(tmdbReq.Host).To(ContainSubstring(testHost))
+		})
+	})
+})
