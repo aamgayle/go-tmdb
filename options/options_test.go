028c5d7b8183fef39bdd91b7ba58d6e48c8c004f
jade-istaken <jade.dergevorkian@gmail.com>
2025-06-10 18:00:01 -0700

    added start and end date option tests

--- options/options_test.go
+++ options/options_test.go
@@ -1,6 +1,8 @@
 package options_test
 
 import (
+	"time"
+
 	"github.com/aamgayle/gotmdb/options"
 	. "github.com/aamgayle/gotmdb/types"
 	. "github.com/onsi/ginkgo/v2"
@@ -68,4 +70,55 @@ var _ = Describe("Options tests", func() {
 			Expect(tmdbReq.URL.Path).To(ContainSubstring("/credit/credit_id"))
 		})
 	})
+
+	//Start and End Date option tests
+
+	Context("When using the EndDate option function", func() {
+		It("should add a properly formatted date to the query", func() {
+			t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+			options := []func(*TMDBReqProps){
+				options.WithBaseURL(protocol + testHost + testSudDirectory),
+				options.WithEndDate(t),
+			}
+			tmdbReq, err := NewTMDBRequest(options...)
+			Expect(err).To(BeNil())
+			Expect(tmdbReq.URL.Path).To(ContainSubstring("&end_date=2009-11-10"))
+		})
+
+		It("should add add padding when single digit days or months are used", func() {
+			t := time.Date(2009, time.January, 5, 23, 0, 0, 0, time.UTC)
+			options := []func(*TMDBReqProps){
+				options.WithBaseURL(protocol + testHost + testSudDirectory),
+				options.WithEndDate(t),
+			}
+			tmdbReq, err := NewTMDBRequest(options...)
+			Expect(err).To(BeNil())
+			Expect(tmdbReq.URL.Path).To(ContainSubstring("&end_date=2009-01-05"))
+		})
+	})
+
+	Context("When using the StartDate option function", func() {
+		It("should add a properly formatted date to the query", func() {
+			t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+			options := []func(*TMDBReqProps){
+				options.WithBaseURL(protocol + testHost + testSudDirectory),
+				options.WithStartDate(t),
+			}
+			tmdbReq, err := NewTMDBRequest(options...)
+			Expect(err).To(BeNil())
+			Expect(tmdbReq.URL.Path).To(ContainSubstring("&start_date=2009-11-10"))
+		})
+
+		It("should add add padding when single digit days or months are used", func() {
+			t := time.Date(2009, time.January, 5, 23, 0, 0, 0, time.UTC)
+			options := []func(*TMDBReqProps){
+				options.WithBaseURL(protocol + testHost + testSudDirectory),
+				options.WithStartDate(t),
+			}
+			tmdbReq, err := NewTMDBRequest(options...)
+			Expect(err).To(BeNil())
+			Expect(tmdbReq.URL.Path).To(ContainSubstring("&start_date=2009-01-05"))
+		})
+	})
+
 })
